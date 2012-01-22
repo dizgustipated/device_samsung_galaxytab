4774934a6a8efa675a6f5c8edc2e1305d0b889de
dizgustipated <dizgustipated@gmail.com>
2012-01-22 12:27:04 -0800

    Update device

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -12,14 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_DEVICE),vzwtab)
+ifeq ($(TARGET_DEVICE),galaxytab)
 LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation, not prelinked and stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lights.c
-
 LOCAL_MODULE_TAGS:= optional
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
