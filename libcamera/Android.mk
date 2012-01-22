db659e3df75bc5b1102b4610fd8058267d286935
dizgustipated <dizgustipated@gmail.com>
2012-01-22 12:22:50 -0800

    Update device

--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -1,4 +1,4 @@
-ifeq ($(TARGET_DEVICE),vzwtab)
+ifeq ($(TARGET_DEVICE),galaxytab)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
@@ -13,7 +13,7 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libs3cjpeg
 LOCAL_SRC_FILES:= \
 	SecCamera.cpp SecCameraHWInterface.cpp
 
-LOCAL_SHARED_LIBRARIES:= libutils libcutils libbinder liblog libcamera_client libhardware
+LOCAL_SHARED_LIBRARIES:= libutils libcutils libbinder liblog libcamera_client libhardwareQLl
 LOCAL_SHARED_LIBRARIES+= libs3cjpeg
 
 LOCAL_MODULE := camera.s5pc110
@@ -23,3 +23,4 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
 endif
+l
