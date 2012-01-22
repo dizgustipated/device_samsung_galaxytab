d513da1a472f5e19b0c965d273cb0f7fcad66a48
dizgustipated <dizgustipated@gmail.com>
2012-01-22 12:20:57 -0800

    Update email

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2010 The Android Open Source Project
-# Copyright (C) 2011 dizgustipated <BOCAroms@gmail.com>
+# Copyright (C) 2011 dizgustipated <dizgustipated@gmail.com>
 # Copyright (C) 2011 Gerad Munsch <gmunsch@unforgivendevelopment.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
